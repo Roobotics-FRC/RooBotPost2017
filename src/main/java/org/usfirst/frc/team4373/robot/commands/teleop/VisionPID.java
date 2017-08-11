1e883b9e63b86b7b610472a3d93364ee9873c245
Henry Pitcairn <735tesla@gmail.com>
2017-08-11 14:45:19 -0400

    Add VisionPID.java

---
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/teleop/VisionPID.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4373.robot.commands.teleop;
+
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import org.usfirst.frc.team4373.robot.subsystems.DriveTrain;
+
+/**
+ * @author (tesla)
+ * Created on 8/11/17
+ */
+public class VisionPID extends PIDCommand {
+    private static double kP = 0.1d;
+    private static double kI = 0.0d;
+    private static double kD = 0.0d;
+    private DriveTrain driveTrain;
+
+    private double pidOutput;
+
+    public static VisionPID visionPID = null;
+
+    public static VisionPID getVisionPID() {
+        return visionPID == null ? visionPID = new VisionPID() : visionPID;
+    }
+
+    public VisionPID() {
+        super("VisionPID", kP, kI, kD);
+    }
+
+    @Override
+    protected double returnPIDInput() {
+        return NetworkTable.getTable("org.usfirst.frc.team4373.vision").getNumber("offset", 0);
+    }
+
+    @Override
+    protected void usePIDOutput(double output) {
+        this.pidOutput = output;
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+        this.driveTrain.setLeft(pidOutput);
+        this.driveTrain.setRight(-pidOutput);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+}
