2ff494355ce280fdf14dd899cb86d167df9b91bf
Rui-Jie Fang <ruijie.fang@temple.edu>
2017-12-09 10:57:54 -0500

    Fix Bob

--- src/main/java/org/usfirst/frc/team4373/robot/commands/LockSubsystemCommand.java
+++ src/main/java/org/usfirst/frc/team4373/robot/commands/LockSubsystemCommand.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team4373.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -12,12 +11,18 @@ public class LockSubsystemCommand extends Command {
     private Subsystem lockedSubsystem;
 
     public LockSubsystemCommand(Subsystem subsystem) {
+        super("Lock" + subsystem.getName() + "Command");
         this.lockedSubsystem = subsystem;
+        requires(this.lockedSubsystem);
+
+    }
+
+    public double getRunningTime() {
+        return this.timeSinceInitialized();
     }
 
     @Override
     protected void initialize() {
-        requires(this.lockedSubsystem);
         setInterruptible(true);
     }
 
